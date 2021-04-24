5a1f3d2de6482d1c87230d6a81a8fe1ebc41367c
wrac4242 <wrac4242@gmail.com>
2021-04-24 20:17:05 +0100

    split code into 2 files

---
+++ src/private_functions.py
@@ -0,0 +1,15 @@
+from hashlib import sha1
+import requests
+
+#makes hash
+#credit where credit is due https://gist.github.com/re4lfl0w/a6ff3a988b4e67297efc
+def make_sha1(initem, encoding='utf-8'):
+    return sha1(initem.encode(encoding)).hexdigest()
+
+#returns hash list
+def make_requests(hash_5, url, parameters):
+    request = requests.get(url+str(hash_5), params=parameters)
+    returning = []
+    for i in request.text.split("\r\n"):
+        returning.append(i[:35])
+    return returning
--- src/wrac_pwd_checker.py
+++ src/wrac_pwd_checker.py
@@ -1,37 +1,23 @@
 """
 module that utilizes haveibeenpwned to give lists of compromised passwords
 """
-from hashlib import sha1
-import requests
+import private_functions as priv
 
 URL = "https://api.pwnedpasswords.com/range/"
 PARAMETERS = {"user-agent":"wrac_password_checker"}
-
-#makes hash
-#credit where credit is due https://gist.github.com/re4lfl0w/a6ff3a988b4e67297efc
-def _make_sha1(initem, encoding='utf-8'):
-    return sha1(initem.encode(encoding)).hexdigest()
-
-#returns hash list
-def _make_requests(hash_5):
-    request = requests.get(URL+str(hash_5), params=PARAMETERS)
-    returning = []
-    for i in request.text.split("\r\n"):
-        returning.append(i[:35])
-    return returning
 
 def compromised_passwords(passwords):
     '''returns a list of compromised passwords from a list of passwords to test'''
     hash_list = []
     to_send = []
     for i in passwords:
-        hashed = _make_sha1(i, encoding='utf-8')
+        hashed = priv.make_sha1(i, encoding='utf-8')
         hash_list.append(hashed)
         to_send.append(hashed[:5])
     to_send = list(set(to_send))
     recived = []
     for i in to_send:
-        recived.extend(_make_requests(i))
+        recived.extend(priv.make_requests(i, URL, PARAMETERS))
     compromised = []
     #check if hashes are in list
     # pylint: disable=consider-using-enumerate
