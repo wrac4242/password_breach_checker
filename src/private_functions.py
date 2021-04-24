363bb859ad5581ca0e0bc4ef4bcc4abc068db459
wrac4242 <wrac4242@gmail.com>
2021-04-24 20:19:16 +0100

    added docstrings

--- src/private_functions.py
+++ src/private_functions.py
@@ -1,13 +1,15 @@
+'''private functions'''
 from hashlib import sha1
 import requests
 
-#makes hash
 #credit where credit is due https://gist.github.com/re4lfl0w/a6ff3a988b4e67297efc
 def make_sha1(initem, encoding='utf-8'):
+    '''makes sha1 hash'''
     return sha1(initem.encode(encoding)).hexdigest()
 
 #returns hash list
 def make_requests(hash_5, url, parameters):
+    '''returns hash list'''
     request = requests.get(url+str(hash_5), params=parameters)
     returning = []
     for i in request.text.split("\r\n"):
