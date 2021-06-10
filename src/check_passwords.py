32523eab0e3d7acdc0c676dfaa2e0fcbfb3f8991
wrac4242 <wrac4242@gmail.com>
2021-06-10 21:37:27 +0100

    added comment info

--- src/check_passwords.py
+++ src/check_passwords.py
@@ -1,6 +1,8 @@
 import wrac_pwd_checker as wrac
-
 from sys import stdin
+
+# some quick code cobbled together
+# takes in passwords from stdin and outputs comprimised ones on stdout
 
 passwds = []
 
